2b7da2696b787410e6f611d6dd6871a04ce4a144
Justus Buyu <jbuyu@ymail.com>
2025-06-06 14:06:56 +0300

    feat(error and loader components):adds error and loader components

--- app/select-skip/page.tsx
+++ app/select-skip/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 import { useWaste } from "@/app/hooks";
+import ErrorComponent from "@/components/shared/error-component";
+import LoaderComponent from "@/components/shared/loader-component copy";
 
 export default function Page() {
   const { data: skips, isLoading, error } = useWaste();
   console.log("skips", skips);
+
+  if (isLoading) {
+    return <LoaderComponent />;
+  }
+  if (error) {
+    return <ErrorComponent error={error} />;
+  }
   return <div>Select Skip</div>;
 }
---
+++ components/shared/error-component.tsx
@@ -0,0 +1,15 @@
+type ErrorComponentProps = {
+  error: unknown; // use `unknown` to safely support both Error and string
+};
+
+export default function Component({ error }: ErrorComponentProps) {
+  return (
+    <div className="text-center py-12">
+      <p className="text-red-600 mb-2">Failed to load skips</p>
+      <p className="text-gray-600 text-sm">Please try again later</p>
+      <p className="text-xs text-gray-500 mt-2">
+        Error: {error instanceof Error ? error.message : "Unknown error"}
+      </p>
+    </div>
+  );
+}
